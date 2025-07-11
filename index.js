e6e3532ad8ef189dd88221ef51dc7f6c0430a6fa
A1rlay <113793274+A1rlay@users.noreply.github.com>
2025-07-10 19:35:19 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+let homePoints = 0;
+let guestPoints = 0;
+
+let homeEl = document.getElementById("home-points");
+let guestEl = document.getElementById("guest-points");
+
+function add1Home(){
+    homePoints += 1;
+    homeEl.textContent = homePoints;
+    checkValue()
+}
+
+function add2Home(){
+    homePoints += 2;
+    homeEl.textContent = homePoints;
+    checkValue()
+}
+
+function add3Home(){
+    homePoints += 3;
+    homeEl.textContent = homePoints;
+    checkValue()
+}
+
+function add1Guest(){
+    guestPoints += 1;
+    guestEl.textContent = guestPoints;
+    checkValue()
+}
+
+function add2Guest(){
+    guestPoints += 2;
+    guestEl.textContent = guestPoints;
+    checkValue()
+}
+
+function add3Guest(){
+    guestPoints += 3;
+    guestEl.textContent = guestPoints;
+    checkValue()
+}
+
+function newGame(){
+    homePoints = 0;
+    guestPoints = 0;
+    homeEl.textContent = homePoints;
+    guestEl.textContent = guestPoints;
+    checkValue()
+}
+
+function checkValue(){
+    if(homePoints == 0 && guestPoints == 0){
+        guestEl.style.textShadow = "none";
+        homeEl.style.textShadow = "none";
+        return;
+    }
+    if(homePoints > guestPoints){
+        homeEl.style.textShadow = "5px 5px 1px #10d981";
+        guestEl.style.textShadow = "none";
+    }
+    else if(homePoints < guestPoints){
+        homeEl.style.textShadow = "none";
+        guestEl.style.textShadow = "5px 5px 1px #10d981";
+    }
+    else{
+        homeEl.style.textShadow = "5px 5px 1px #10d981";
+        guestEl.style.textShadow = "5px 5px 1px #10d981";
+    }
+}
